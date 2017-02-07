8f4c12a6651835a0cb19d628148331a561b12d71
ByeongSu Do <bsdo@naver.com>
2017-02-07 17:21:51 +0900

    first

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Created by dobyeongsu on 2017. 2. 7..
+ */
+const Ajv = require('ajv');
+const { collapseWhitespace } = require('strman');
+
+// option `i18n` is required for this package to work
+const ajv = new Ajv({
+  useDefaults: true
+}); // options can be passed, e.g. {allErrors: true}
+
+const schema = {
+  type: 'object',
+  properties: {
+    de: { type: 'string', default: '3' },
+    strings: { type: 'string' },
+    test: { type: 'number' },
+  }
+};
+
+const data = {
+  test: 1,
+  strings: collapseWhitespace(1)
+};
+
+const validate = ajv.compile(schema);
+const valid = validate(data);
+
+if (!valid) {
+
+  console.log(validate.errors);
+
+}
+
+console.log(data);
